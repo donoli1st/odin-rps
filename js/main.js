309e8dde32f6c6e5045d065a9e9b3ce7dba17dc0
donoli1st <oliverzuellig@gmail.com>
2025-11-08 00:50:35 +0100

    Implements getHumanChoice funciton

--- js/main.js
+++ js/main.js
@@ -1,14 +1,38 @@
+const decisions = [
+    'rock',
+    'papaer',
+    'scissors'
+]
 
+let humanScore = 0;
+let computerScore = 0;
 
 function getComputerChoice(){
     let ran = Math.random();
 
     if(ran < 0.33){
-        return 'rock';
+        return decisions[0];
     } else if(ran < 0.66){
-        return 'papaer';
+        return decisions[1];
     } else {
-        return 'scissors';
+        return decisions[2];
     }
 }
 
+function getHumanChoice(){
+    let userPrompt = prompt('Chose rock, papaer or scissors', decisions[0]).toLowerCase();
+
+    if(!decisions.includes(userPrompt)){
+        alert('Use rock, paper or scissors as answer.');
+        restartGame();
+        return;
+    }
+
+    
+}
+
+function restartGame (){
+    window.location.reload();
+}
+
+getHumanChoice();
